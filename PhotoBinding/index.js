f88c4204254b37f66c4ad83d849d51ace22c7fda
Phạm Cao Sang <pcs@netcompany.com>
2023-08-14 21:48:07 +0700

    add queue output for photobinding

--- PhotoBinding/index.js
+++ PhotoBinding/index.js
@@ -3,6 +3,7 @@ const imageHandler = require("../core/imageHandler")
 module.exports = async function (context) {
     try {
         const { data } = await imageHandler(context)
+        context.bindings.photoCognitiveResult = { data, path: context.bindingData.blobTrigger }
         context.log("result", JSON.stringify(data))
     } catch ({ err: { response: { data, status } } }) {
         context.log("err", { data, status })
--- ProfileBinding/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function (context) {
-    context.log("Full blob path:", context.bindingData.blobTrigger);
-    context.log('Node.js Blob trigger function processed', context.bindings.myProfile);
-};
