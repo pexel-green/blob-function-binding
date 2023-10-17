7defcbff6050a18b611e96a9ce4ce1b5f8bceafd
Phạm Cao Sang <pcs@netcompany.com>
2023-10-17 21:36:38 +0700

    enhance

--- PhotoBinding/index.js
+++ PhotoBinding/index.js
@@ -2,10 +2,12 @@ const imageHandler = require("../core/imageHandler")
 
 module.exports = async function (context) {
     try {
+        context.log("Receive request:", context.bindingData)
         const { data } = await imageHandler(context)
         context.bindings.photoCognitiveResult = { data, path: context.bindingData.blobTrigger }
-        context.log("result", JSON.stringify(context.bindings.photoCognitiveResult))
-    } catch ({ err: { response: { data, status } } }) {
-        context.log("err", { data, status })
+        context.log("Result:", context.bindings.photoCognitiveResult)
+    }
+    catch ({ err: { response: { data, status } } }) {
+        context.log.error("Error:", { data, status })
     }
 };
