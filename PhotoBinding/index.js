e54e8d9ac6339c92e2c8162281e834f4c9f5bb1a
Phạm Cao Sang <pcs@netcompany.com>
2023-10-17 19:28:36 +0700

    change log result

--- PhotoBinding/index.js
+++ PhotoBinding/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context) {
     try {
         const { data } = await imageHandler(context)
         context.bindings.photoCognitiveResult = { data, path: context.bindingData.blobTrigger }
-        context.log("result", JSON.stringify(data))
+        context.log("result", JSON.stringify(context.bindings.photoCognitiveResult))
     } catch ({ err: { response: { data, status } } }) {
         context.log("err", { data, status })
     }
